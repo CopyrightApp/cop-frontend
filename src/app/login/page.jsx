0546282c366dcb2b2e7de0ea59a098b4c6427d9b
Santi4go32 <quierocarne40@gmail.com>
2024-05-31 17:12:48 -0500

    Global Languages - V1

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -59,11 +59,11 @@ function Login() {
                             </h1>
                             <form className="space-y-4 md:space-y-6">
                                 <div>
-                                    <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Your email</label>
+                                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Your email</label>
                                     <input type="email" name="email" id="email" className="border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
                                 </div>
                                 <div>
-                                    <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Password</label>
+                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Password</label>
                                     <input type="password" name="password" id="password" placeholder="••••••••" className="border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
                                 </div>
                                 <div className="flex items-center justify-between">
@@ -72,7 +72,7 @@ function Login() {
                                             <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
                                         </div>
                                         <div className="ml-3 text-sm">
-                                            <label for="remember" className="text-gray-500">Remember me</label>
+                                            <label htmlFor="remember" className="text-gray-500">Remember me</label>
                                         </div>
                                     </div>
                                     <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
@@ -84,7 +84,7 @@ function Login() {
                                     <svg className="h-6 w-6 mr-2" xmlns="http://www.w3.org/2000/svg"
                                         viewBox="-0.5 0 48 48" version="1.1">
 
-                                        <g id="Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                        <g id="Icons" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
                                             <g id="Color-" transform="translate(-401.000000, -860.000000)">
                                                 <g id="Google" transform="translate(401.000000, 860.000000)">
                                                     <path
@@ -109,10 +109,10 @@ function Login() {
                                     className="flex items-center bg-white border border-gray-300 rounded-lg shadow-md max-w-xs px-6 py-2 text-sm font-medium text-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
                                     <svg className="h-6 w-6 mr-2" xmlns="http://www.w3.org/2000/svg"
                                         viewBox="0 0 73 73" version="1.1">
-                                        <g id="team-collaboration/version-control/github" stroke="none" stroke-width="1" fill="none"
-                                            fill-rule="evenodd">
-                                            <g id="container" transform="translate(2.000000, 2.000000)" fill-rule="nonzero">
-                                                <rect id="mask" stroke="#000000" stroke-width="2" fill="#000000" x="-1"
+                                        <g id="team-collaboration/version-control/github" stroke="none" strokeWidth="1" fill="none"
+                                            fillRule="evenodd">
+                                            <g id="container" transform="translate(2.000000, 2.000000)" fillRule="nonzero">
+                                                <rect id="mask" stroke="#000000" strokeWidth="2" fill="#000000" x="-1"
                                                     y="-1" width="71" height="71" rx="14">
 
                                                 </rect>
